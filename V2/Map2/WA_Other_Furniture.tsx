a03379c861900eabeedfc62d8ba3ef9adbaf096a
jotto1 <johannes.otto@stud.htwk-leipzig.de>
2023-06-27 21:01:42 +0000

    Update V2/Map2/Sign1.tsx, V2/Map2/WA_Decoration.tsx, V2/Map2/WA_Exterior.tsx, V2/Map2/WA_Miscellaneous.tsx, V2/Map2/WA_Other_Furniture.tsx, V2/Map2/WA_Room_Builder.tsx, V2/Map2/WA_Seats.tsx, V2/Map2/WA_Special_Zones.tsx, V2/Map2/WA_Tables.tsx, V2/Map2/WA_User_Interface.tsx, V2/Map2/ghost all.tsx, V2/Map2/microphone.tsx, V2/Map2/pixil-frame-0 (2).tsx, V2/Map2/sign 2.tsx, V2/Map2/wateranimate2.tsx

--- V2/Map2/WA_Other_Furniture.tsx
+++ V2/Map2/WA_Other_Furniture.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="WA_Other_Furniture" tilewidth="32" tileheight="32" tilecount="156" columns="12">
- <image source="../tileset/WA_Other_Furniture.png" width="384" height="416"/>
+ <image source="tileset/WA_Other_Furniture.png" width="384" height="416"/>
 </tileset>
--- V2/Map2/WA_Tables.tsx
+++ V2/Map2/WA_Tables.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="WA_Tables" tilewidth="32" tileheight="32" tilecount="270" columns="10">
- <image source="../tileset/WA_Tables.png" width="320" height="864"/>
+ <image source="tileset/WA_Tables.png" width="320" height="864"/>
 </tileset>
