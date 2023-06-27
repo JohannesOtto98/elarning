5cd72c725dcc6954ef7530bf453998e7934a9ff4
jotto1 <johannes.otto@stud.htwk-leipzig.de>
2023-06-27 22:46:05 +0200

    v2

---
+++ V2/Map2/wateranimate2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="wateranimate2" tilewidth="32" tileheight="32" tilecount="216" columns="18">
+ <image source="C:/Users/Hans/Downloads/wateranimate2.png" width="576" height="386"/>
+</tileset>
