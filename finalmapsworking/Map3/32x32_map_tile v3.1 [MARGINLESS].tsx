ef30358ef9627b917f7b9a35ea209a570e74fbf7
jotto1 <johannes.otto@stud.htwk-leipzig.de>
2023-07-06 21:57:05 +0200

    v3

---
+++ finalmapsworking/Map3/32x32_map_tile v3.1 [MARGINLESS].tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="32x32_map_tile v3.1 [MARGINLESS]" tilewidth="32" tileheight="32" tilecount="667" columns="23">
+ <image source="C:/Users/Hans/Downloads/32x32_map_tile v3.1 [MARGINLESS].png" width="736" height="928"/>
+</tileset>
