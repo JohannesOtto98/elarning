08b41b0aaef80d4e4bbd43414d6c26c5bcfefe01
jotto1 <johannes.otto@stud.htwk-leipzig.de>
2023-07-09 11:27:51 +0200

    v3

--- Map_Base/mic2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="mic2" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="C:/Users/Hans/Downloads/pixil-frame-0 (1).png" width="32" height="32"/>
-</tileset>
